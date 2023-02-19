a11666c75bb7a970ca1543a17bd22eb19ef55f82
victorglim <victor.guedes888@gmail.com>
2023-02-18 21:39:31 -0300

    fix: resolve bug response in post users register return json hashpassword

--- src/services/createUsers.service.ts
+++ src/services/createUsers.service.ts
@@ -1,6 +1,6 @@
 import format from "pg-format";
 import { client } from "../database";
-import { IUserResult, IUserWithoutPassword, IUserRequest } from "../interfaces/users.interfaces";
+import { IUserResult, IUserWithoutPassword, IUserRequest, IUserReq } from "../interfaces/users.interfaces";
 import { QueryConfig } from "pg";
 
 export const createUsersService = async (payload: IUserRequest): Promise<IUserWithoutPassword | any> => {
@@ -29,8 +29,28 @@ export const createUsersService = async (payload: IUserRequest): Promise<IUserWi
             values: [payload.name, payload.email, payload.password, adm],
         };
 
+
         const userInfoResult: IUserResult = await client.query(queryConfig);
 
-        return userInfoResult.rows[0];
+        const selectString: string = `
+            SELECT 
+                id, name, email, admin, active
+            FROM 
+                users
+            WHERE
+                email = $1;
+ `
+ 
+    const queryStrSelect: QueryConfig = {
+      text: selectString,
+      values: [userInfoResult.rows[0].email]
+    }
+
+
+    const querySelect: IUserReq = await client.query(queryStrSelect);
+  
+    return querySelect.rows[0];
+
+      
    
 };
